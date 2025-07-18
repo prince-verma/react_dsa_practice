0071a3bb1f64311f2c5b5a852e7a485f46fd19fa
prince-verma <prince081293@gmail.com>
2025-07-18 08:35:09 +0530

    feat: implement joke fetching functionality and update UI to display jokes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import { pattern1 } from "./DSA/patterns/wPattern";
 // import { MaxHeap } from "./DSA/tree/maxHeap";
 // import { MinHeap } from "./DSA/tree/minHeap";
 // import { heapSort } from "./DSA/array/heapSort";
 // import { buildBinaryTree } from "./DSA/tree/buildBinaryTree_InOrder_preOrder";
 // import * as basics from './typeScript/basics'
 
-// const range = {
-//   from: 1,
-//   to: 5,
-//   [Symbol.asyncIterator]: async function* () {
-//     try {
-//       for (let i = this.from; i <= this.to; i++) {
-//         await new Promise((resolve) => setTimeout(resolve, 1000));
-//         yield i;
-//       }
-//     } catch (error) {
-//       console.error("Caught an error:", error.message);
-//     }
-//   },
-// };
+const getJoke = async () => {
+  try {
+    const url = "https://api.chucknorris.io/jokes/random";
+    const data = await fetch(url);
+    const joke = await data.json();
+    console.log("data", joke);
+    return joke.value;
+  } catch (error) {
+  }
+};
 
 function App() {
+  const [joke, setJoke] = React.useState("");
+
   React.useEffect(() => {
-    // console.log(`here comes in sum --  ${stringSum("1", "2")}`);
-    // console.log(`here comes in sum --  ${stringSum("98765", "12345")}`);
-    // console.log(`here comes in sum --  ${stringSum("16385", "13745")}`);
-    // console.log(`here comes in sum --  ${stringSum("18", "12.10")}`);
-    // console.log(`here comes in sum --  ${stringSum("12.1", "12.10")}`);
-    // console.log(`here comes in sum --  ${stringSum("12.80", "12.10")}`);
-    // console.log(`here comes in sum --  ${stringSum("12.911", "12.10")}`);
-    // console.log(`here comes in sum --  ${stringSum("12345.123", "29876.123")}`);
-    // console.log(`here comes in sum --  ${sum(1)(2)}`);
-    // console.log(`here comes in range --  ${JSON.stringify(range)}`);
-    // console.log("here comes in -- ", import.meta.url); // script URL
-    // (async () => {
-    //   for await (const value of range) {
-    //     console.log(`here comes in value -- `, value);
-    //   }
-    // })();
     // MyPromise.resolve(10).then((x) => {
     //   console.log('x -1 -', x);
     //   return x + 10;
@@ -62,19 +43,17 @@ function App() {
     // });
   }, []);
 
+  const handCtaClick = async () => {
+    const data = await getJoke();
+    setJoke(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <pre>{pattern1(11)}</pre>
+        {joke}
+        <button onClick={handCtaClick}>click me to get a joke</button>
       </header>
     </div>
   );
